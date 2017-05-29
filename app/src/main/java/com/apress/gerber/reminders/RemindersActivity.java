8cda0b1f829dd7400fb48847eb5bde056173c628
KangMax <1365888636@qq.com>
2017-05-30 00:33:39 +0800

    样式1

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -2,12 +2,25 @@ package com.apress.gerber.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class RemindersActivity extends AppCompatActivity {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+        mListView = (ListView) findViewById(R.id.reminders_list_view);
+        //The arrayAdatper is the controller in our
+        //model-view-controller relationship. (controller)
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(
+                this,
+                R.layout.reminders_row,
+                R.id.row_text,
+                new String[]{"first record", "second record", "third record"});
+        mListView.setAdapter(arrayAdapter);
     }
 }
