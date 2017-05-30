f44813c0acd6636e55754bed8696b7bbe06429c9
KangMax <1365888636@qq.com>
2017-05-30 22:33:57 +0800

    Adds Example reminders

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -23,6 +23,26 @@ public class RemindersActivity extends AppCompatActivity {
         mListView.setDivider(null);
         mDbAdapter=new RemindersDbAdapter(this);
         mDbAdapter.open();
+
+        if(savedInstanceState==null){
+            mDbAdapter.deleteAllReminders();
+            insertSomeReminders("Buy Learn Android Studio", true);
+            insertSomeReminders("Send Dad birthday gift", false);
+            insertSomeReminders("Dinner at the Gage on Friday", false);
+            insertSomeReminders("String squash racket", false);
+            insertSomeReminders("Shovel and salt walkways", false);
+            insertSomeReminders("Prepare Advanced Android syllabus", true);
+            insertSomeReminders("Buy new office chair", false);
+            insertSomeReminders("Call Auto-body shop for quote", false);
+            insertSomeReminders("Renew membership to club", false);
+            insertSomeReminders("Buy new Galaxy Android phone", true);
+            insertSomeReminders("Sell old Android phone - auction", false);
+            insertSomeReminders("Buy new paddles for kayaks", false);
+            insertSomeReminders("Call accountant about tax returns", false);
+            insertSomeReminders("Buy 300,000 shares of Google", false);
+            insertSomeReminders("Call the Dalai Lama back", true);
+        }
+
         Cursor cursor=mDbAdapter.fetchAllReminders();
         //from columns defined in the db
         String[] from=new String[]{RemindersDbAdapter.COL_CONTENT};
@@ -35,6 +55,10 @@ public class RemindersActivity extends AppCompatActivity {
         mListView.setAdapter(mCursorAdapter);
         //The arrayAdatper is the controller in our
         //model-view-controller relationship. (controller)
+    }
+
+    private void insertSomeReminders(String name, boolean important) {
+        mDbAdapter.createReminder(name, important);
     }
 
     @Override
