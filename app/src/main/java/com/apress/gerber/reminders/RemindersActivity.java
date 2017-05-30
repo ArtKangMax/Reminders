8cfebc72daf1791ee68dee1300fa10e7fa93b5ee
KangMax <1365888636@qq.com>
2017-05-30 20:28:24 +0800

    Adds new reminder and exit menu options

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -2,6 +2,9 @@ package com.apress.gerber.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,11 +19,31 @@ public class RemindersActivity extends AppCompatActivity {
         mListView = (ListView) findViewById(R.id.reminders_list_view);
         //The arrayAdatper is the controller in our
         //model-view-controller relationship. (controller)
-        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
                 R.layout.reminders_row,
                 R.id.row_text,
                 new String[]{"first record", "second record", "third record"});
         mListView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_reminders, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                Log.d(getLocalClassName(), "create new Reminder");
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+            default:
+                return false;
+        }
     }
 }
